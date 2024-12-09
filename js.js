0ae8f8cf56fe302249180f1cd84a5ca2ebba2e31
Aset <asetles0@gmail.com>
2024-12-09 10:55:57 +0500

    first commit

---
+++ js.js
@@ -0,0 +1,19 @@
+let name = 'kirsi dukenge';
+let phoneNumber = 87470585178;
+let products = ['butter', 'bread', 'milk', 'water', 'fig'];
+let productName = 'products'
+let category = [
+    { aziq: "butter" },
+    { tylik: "milk" },
+    { dukeni: "fig" },
+]  
+let price = 4230;
+let stock = phoneNumber.lengs;
+function change() { 
+    changeShopNumbe = prompt("Dukennin atin ozgert");
+    return (name = changeShopNumbe);
+}
+
+console.log(change());
+
+let changePhoneNumber = +prompt("Nomerdi ozgert");
